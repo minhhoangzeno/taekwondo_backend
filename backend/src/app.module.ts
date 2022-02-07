d5c5450b0323eae92bca667c71ef0944e38ac55e
minhhoangzeno <minhhoangzeno@gmail.com>
2022-02-07 14:44:04 +0700

    update app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BlogModule } from './blog/blog.module';
-import { CategoryVideoController } from './category-video/category-video.controller';
-import { CategoryVideoService } from './category-video/category-video.service';
-import { CategoryVideoModule } from './category-video/category-video.module';
-import { VideoModule } from './video/video.module';
-import { CommentModule } from './comment/comment.module';
-import { ReplyModule } from './reply/reply.module';
-import { ForumModule } from './forum/forum.module';
 import { CategoryForumModule } from './category-forum/category-forum.module';
-import { APP_GUARD } from '@nestjs/core';
+import { CategoryVideoModule } from './category-video/category-video.module';
+import { CommentModule } from './comment/comment.module';
+import { ForumModule } from './forum/forum.module';
+import { ReplyModule } from './reply/reply.module';
 import { RolesGuard } from './role/roles.guard';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
   imports: [UserModule,
-    MongooseModule.forRoot('mongodb://localhost/taekwondo'),
+    MongooseModule.forRoot('mongodb+srv://minhhoangzeno:<password>@cluster0.lgkmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
     AuthModule,
     BlogModule,
     CategoryVideoModule,
